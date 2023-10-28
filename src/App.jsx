a73ef92041ddd9bce3a959e8885887d26463a934
gyanendra kumar bais <gyanedrabais54@gmail.com>
2023-10-28 15:23:59 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+ return( <><h1 className="text-red-500">gyanendra kumar bais</h1>
+ <p className="text-yellow-300">my father name is mrs gend kunvar devi bais</p>
+ </>)
+}
