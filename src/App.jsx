3eb7761dcd4bb8589c01371cdc4bd61ef883f47b
gyanendra kumar bais <gyanedrabais54@gmail.com>
2023-10-29 12:23:58 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
+
 export default function App() {
- return( <><h1 className="text-red-500">gyanendra kumar bais</h1>
- <p className="text-yellow-300">my father name is mrs gend kunvar devi bais</p>
- </>)
+  return (
+ 
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+   <Route path='/sign-in' element={<SignIn/>}/>
+   <Route path='/sign-Up' element={<SignUp/>}/>
+   <Route path='/about' element={<About/>}/>
+   <Route path='/profile' element={<Profile/>}/>
+  </Routes>
+  </BrowserRouter>
+  )
 }
