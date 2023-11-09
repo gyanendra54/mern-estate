8a1289f95a0a328a728c999376ac025128b9046a
gyanendra kumar bais <gyanedrabais54@gmail.com>
2023-11-09 21:33:24 +0530

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,5 +58,14 @@ export const google = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-}
+};
+
+export const signOut =async(req, res, next)=>{
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been looged out!');
+  } catch (error) {
+    next(error);
+  }
+};
 
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -43,6 +43,18 @@ reducers: {
   deleteUserFailure:(state, action)=>{
     state.error=action.payload;
     state.loading=false;
+  },
+  signOutUserStart: (state)=>{
+    state.loading=true;
+  },
+  signOutUserSuccess: (state)=>{
+    state.currentUser=null;
+    state.loading=false;
+    state.error=null;
+  },
+  signOutUserFailure: (state, action)=>{
+    state.error=action.payload;
+    state.loading=false;
   }
 },
 });
@@ -56,6 +68,9 @@ export const {
     deleteUserFailure,
     deleteUserSuccess,
     deleteUserStart,
+    signOutUserFailure,
+    signOutUserSuccess,
+    signOutUserStart,
   } = userSlice.actions;
   
   export default userSlice.reducer;
