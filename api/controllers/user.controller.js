32908acbd273fab777dc82cfc141f8f582ca6d9f
gyanendra kumar bais <gyanedrabais54@gmail.com>
2023-11-14 15:37:56 +0530

    complete show user listings functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -36,28 +36,27 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
-
-  export const deleteUser=async(req, res, next)=>{
-    if(req.user.id !==req.params.id)
-    return next(errorHandler(401, 'you can only delete your own account!'))
+  export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, 'You can only delete your own account!'));
     try {
       await User.findByIdAndDelete(req.params.id);
       res.clearCookie('access_token');
-      res.status(200).json('user has been deleted!');
+      res.status(200).json('User has been deleted!');
     } catch (error) {
       next(error);
     }
   };
-  export const getUserListings=async (req, res, next)=>{
-    if(req.user.id===req.params.id)
-    {
+
+  export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
       try {
-        const listings=await Listing.find({userRef:req.params.id});
+        const listings = await Listing.find({ userRef: req.params.id });
         res.status(200).json(listings);
       } catch (error) {
-        next(error)
+        next(error);
       }
-    }else{
-      return next(errorHandler(401, 'you can only view your own listings!'));
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
     }
   };
